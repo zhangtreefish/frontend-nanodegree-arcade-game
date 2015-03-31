12ed188c2c6dea09fcd57897c088b0d01128860a
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-03-30 21:45:20 -0500

    add Player class, add Player prototype methods, all with console.log inside; add allEnemies as array; add console.log in Enemy.update method.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    console.log("wow");
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,10 +25,23 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+var Player = function(){
+    this.sprite ="images/char-pink-girl.png";
+}
+Player.prototype.update=function(){
+    console.log("Player-update");
+}
+Player.prototype.render=function(){
+    console.log("player-render");
+}
+Player.prototype.handleInput=function(){
+    console.log("player-handleInput");
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies=[];
+allEnemies[0]=new Enemy();
+var player=new Player();
 // Place the player object in a variable called player
 
 
